19e91fcfb448a49c57e579a00a8f8b81f9782def
christykazi <christyagbai4178@gmail.com>
2023-08-28 16:42:32 +0100

    "building  airbnb"

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
         <li> <Link to="/youtube">Youtube</Link></li>
         <li> <Link to="/netflix">Netflix</Link></li>
         <li> <Link to="/airbnb">Airbnb</Link></li>
-        <br />
+        
             <li> <Link to="/projectone">Project 1</Link></li>
            <li> <Link to="/projectwo"> Project 2</Link></li>
            <li> <Link to="/projecthree">Project 3</Link></li>
