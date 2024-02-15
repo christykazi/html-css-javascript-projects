ffbc9be3af37886961f8593a14a6b68f9c470d71
christykazi <christyagbai4178@gmail.com>
2024-02-15 23:23:18 +0100

    debugging

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Linkedin from "./linkedin/Linkedin";
 import Youtube from "./youtube/Youtube";
 import Netflix from "./netflix/Netflix";
 import Airbnb from "./airbnb/Airbnb";
+import Start from "./start/Start";
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
         <Route path="/projecthree" element = {<Projecthree/>}/>
         <Route path="/projectfour" element = {<Projectfour/>}/>
         <Route path="/facebook" element = {<Facebook/>}/>
+        <Route path="/start" element = {<Start/>}/>
         <Route path="/linkedin" element = {<Linkedin/>}/>
         <Route path="/youtube" element = {<Youtube/>}/>
         <Route path="/netflix" element = {<Netflix/>}/>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -10,15 +10,16 @@ const Home = () => {
         <h2 className='hea'> My Unique Projects</h2>
         <div className="navbar">
         <ul>
-        <li> <Link to="/facebook">FaceBook</Link></li>
-        <li> <Link to="/linkedin">Linkedin</Link></li>
-        <li> <Link to="/youtube">Youtube</Link></li>
-        <li> <Link to="/netflix">Netflix</Link></li>
-        <li> <Link to="/airbnb">Airbnb</Link></li>
+        <li> <Link className='aa'
+        to="/facebook">FaceBook</Link></li>
+        <li> <Link className='aa' to="/linkedin">Linkedin</Link></li>
+        <li> <Link className='aa' to="/youtube">Youtube</Link></li>
+        <li> <Link className='aa' to="/netflix">Netflix</Link></li>
+        <li> <Link className='aa' to="/airbnb">Airbnb</Link></li>
         
-            <li> <Link to="/projectone">Project 1</Link></li>
-           <li> <Link to="/projectwo"> Project 2</Link></li>
-           <li> <Link to="/projecthree">Project 3</Link></li>
+            <li> <Link className='aa' to="/projectone">Project 1</Link></li>
+           <li> <Link className='aa' to="/projectwo"> Project 2</Link></li>
+           <li> <Link className='aa' to="/projecthree">Project 3</Link></li>
       
        
              
@@ -26,8 +27,8 @@ const Home = () => {
         <div className='content'>
         <h1>Welcome to my Abode </h1>
         <div>
-        <button type="button" ><span></span>CLICK  ALL</button>
-            <button type="button"><span></span>PROJECTS ABOVE</button>
+        <button type="button" className='button4'><span></span>CLICK  ALL</button>
+            <button type="button" className='button4'><span></span>PROJECTS ABOVE</button>
         </div>
            
         </div>
