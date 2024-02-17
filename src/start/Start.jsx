2e7df8728bac1c4c7534fea8f7cbdbcd6eb63349
christykazi <christyagbai4178@gmail.com>
2024-02-17 17:09:14 +0100

    new update

--- src/start/Start.jsx
+++ src/start/Start.jsx
@@ -114,7 +114,7 @@ const StartCompontent = () => {
           </div>
           <div className="story story3">
             <img src={MEM1}/>
-            <p className='ncc'>Onyeka Gift</p>
+            <p className='ncc'>Agbai Christy</p>
           </div>
           <div className="story story4">
             <img src={MEM2} />
