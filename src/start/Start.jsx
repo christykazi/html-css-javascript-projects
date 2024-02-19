09abb2ca288051083ccc7109cf11a740ec6df539
christykazi <christyagbai4178@gmail.com>
2024-02-19 19:42:53 +0100

    update

--- src/start/Start.jsx
+++ src/start/Start.jsx
@@ -112,15 +112,15 @@ const StartCompontent = () => {
           </div>
           <div className="story story2">
             <img src={MEM3} />
-            <p className='ncc'>Uche princess</p>
+            <p className='ncc'>Agbai Christy</p>
           </div>
           <div className="story story3">
             <img src={MEM1}/>
-            <p className='ncc'>Agbai Christy</p>
+            <p className='ncc'>Uche princess</p>
           </div>
           <div className="story story4">
             <img src={MEM2} />
-            <p className='ncc'> Felis keth</p>
+            <p className='ncc'>Felis keth</p>
           </div>
           <div className="story story5">
             <img src={MEM4} />
